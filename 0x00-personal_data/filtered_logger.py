da0da670f0923de38000f71e041434a4047f0a60
Dan <danlewismuriuki@gmail.com>
2024-08-29 18:12:47 +0300

    Implement RedactingFormatter class for PII obfuscation
    - Updated RedactingFormatter to accept a list of fields for redaction.
    - Implemented the format method to use the filter_datum function for filtering values in log messages.
    - Ensured that specified fields are redacted in the formatted log output.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ import re
 import logging
 from typing import List, Tuple
 
-import mysql.connector
+# import mysql.connector
 
 
 PII_FIELDS: Tuple[str] = ('name', 'email', 'phone', 'ssn', 'password')
@@ -24,3 +24,24 @@ def filter_datum(
         pattern = r'({0}=)[^{1}]*({1})'.format(key, separator)
         message = re.sub(pattern, r'\1{}\2'.format(redaction), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            message,
+            self.SEPARATOR)
