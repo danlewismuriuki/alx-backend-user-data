72ff55de2f3382f07d12c3c93569bc92f537b3b7
Dan <danlewismuriuki@gmail.com>
2024-09-03 22:55:46 +0300

    Add docstring to extract_base64_authorization_header method
    Included documentation for the extract_base64_authorization_header method in the BasicAuth class. The docstring describes the method's purpose, parameters, and return values.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """ Basic Authentication module
 """
 
@@ -13,6 +14,14 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """
+        Extracts the Base64 part from the Authorization
+        header for Basic Authentication.
+
+        This method checks if the authorization_header is valid,
+        starts with "Basic ",
+        and extracts the Base64 encoded credentials part of the header.
+        """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
