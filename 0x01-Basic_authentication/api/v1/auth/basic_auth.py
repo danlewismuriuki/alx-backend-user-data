8d8bddcdfacd02f13fb2df782a0c64a80bebc54f
Dan <danlewismuriuki@gmail.com>
2024-09-03 23:20:27 +0300

    Fix Base64 Decoding with Robust Error Handling
    - Added exception handling for `TypeError`, `ValueError`, and `UnicodeDecodeError` in the Base64 decoding function.
    - `TypeError` is caught to handle invalid input types.
    - `ValueError` is caught to manage cases of incorrect Base64 padding or malformed input.
    - `UnicodeDecodeError` is caught to handle invalid UTF-8 sequences during string conversion.
    
    This ensures that the function gracefully returns `None` for erroneous inputs and improves overall robustness and reliability.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -30,3 +31,18 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(
+                base64_authorization_header, validate=True)
+            return decoded_bytes.decode('utf-8')
+        except (TypeError, ValueError, UnicodeEncodeError):
+            return None
