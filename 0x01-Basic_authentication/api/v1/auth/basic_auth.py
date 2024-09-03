80a6fdbeddb419204ff0e7d35e2a016b5f9fcf8a
Dan <danlewismuriuki@gmail.com>
2024-09-04 00:33:16 +0300

    Add user authentication in BasicAuth class
    - Implemented user lookup and password validation in the `user_object_from_credentials` method.
    - Added error handling to manage exceptions during user search.
    - Ensured the method returns `None` if no matching user is found or an error occurs.
    - Validated user credentials by checking the email and password against stored data.
    
    This update enables the BasicAuth class to authenticate users based on email and password.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -79,3 +81,24 @@ class BasicAuth(Auth):
 
         email, password = parts
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Retrieves a User instance based on email and password credentials.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
