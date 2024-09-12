e1609bae44af57a4816d083039f4cffbe9d68aa2
Dan <danlewismuriuki@gmail.com>
2024-09-12 13:24:18 +0300

    Add logout functionality for session management
    - Implemented `/sessions` endpoint with DELETE method for user logout.
    - Extracted the session ID from cookies and used it to retrieve the user.
    - Destroyed the user's session and redirected to the homepage upon successful logout.
    - Responded with a 403 HTTP status if the session ID is missing or user does not exist.
    
    Ensures proper session handling and user redirection after logout.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """API Routes for Authentication Service"""
 from flask import jsonify, Flask, request, make_response, abort
+from flask import redirect
 from auth import Auth
 # from typing import bool
 app = Flask(__name__)
@@ -52,6 +53,23 @@ def login():
         jsonify({"email": email, "message": "logged in"}))
     response.set_cookie("session_id", session_id)
     return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """Find the user with the requested session ID.
+    If the user exists destroy the session and redirect the user to GET /.
+    If the user does not exist, respond with a 403 HTTP status.
+    """
+
+    session_id = request.cookies.get("session_id", None)
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
