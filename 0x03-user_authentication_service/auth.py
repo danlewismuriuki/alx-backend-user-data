6fa068bd5458affa1be66a4a21bd153a78cfa8f3
Dan <danlewismuriuki@gmail.com>
2024-09-14 12:17:29 +0300

    feat: implement destroy_session method to invalidate user session
    - Added `destroy_session` method in the Auth class to update the user's session ID to None
    - Used `find_user_by` to fetch the user by ID and `update_user` to set the session ID to None
    - Handled case where user is not found by catching `NoResultFound` exception

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,10 +97,9 @@ class Auth:
         """Updates the corresponding user's session ID to None"""
         try:
             user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
-
-        self._db.update_user(user.id, session_id=None)
 
         return None
 
